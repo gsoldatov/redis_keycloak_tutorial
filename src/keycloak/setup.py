37ac4a6924842748a0240c774067860151dedbf9
-- <smoke571reg@yandex.ru>
2025-08-10 18:00:02 +0300

    Added Keycloak app realm setup

---
+++ src/keycloak/setup.py
@@ -0,0 +1,200 @@
+from functools import wraps
+from time import sleep
+
+from keycloak import KeycloakAdmin
+import httpx
+
+from typing import Self, Any
+
+# TODO delete
+if __name__ == "__main__":
+    from pathlib import Path
+    import sys
+    project_root = Path(__file__).parent.parent.parent
+    sys.path.insert(0, str(project_root))
+
+    from config import load_config
+
+from config import KeycloakConfig
+
+
+def reset_keycloak_app_realm(kc_config: KeycloakConfig):
+    """
+    Deletes existing app realm, creates a new one 
+    and adds app client, roles and test users into it.
+    """
+    with KeycloakManager(kc_config) as manager:
+        manager.delete_app_realm()
+
+        manager.create_app_realm()
+        manager.create_app_client()
+        manager.add_client_role("role-1")
+        manager.add_client_role("role-2")
+
+        user_1_id = manager.add_user("user-1", "password", ["role-1"])
+        user_2_id = manager.add_user("user-2", "password", ["role-2"])
+        superuser_id = manager.add_user("superuser", "password", ["role-1", "role-2"])
+
+
+def in_app_realm(fn):
+    """
+    Decorator for KeycloakManager methods, which ensures,
+    that app realm is selected as current realm when a method is called.
+    """
+    @wraps(fn)
+    def inner(self: "KeycloakManager", *args, **kwargs) -> Any:
+        current_realm = self.admin.get_current_realm()
+        self.admin.change_current_realm(self.kc_config.app_realm_name)
+        result = fn(self, *args, **kwargs)
+        self.admin.change_current_realm(current_realm)
+        return result
+    
+    return inner
+
+
+class KeycloakManager:
+    def __init__(self, kc_config: KeycloakConfig):
+        self.kc_config = kc_config
+        self.admin = KeycloakAdmin(
+            server_url=f"http://localhost:{kc_config.container_main_port}",
+            username=kc_config.admin_username,
+            password=kc_config.admin_password,
+            realm_name="master"
+        )
+        self._app_client_id: str | None = None
+    
+    def wait_for_server(self) -> None:
+        """
+        Waits for Keycloak server healthcheck endpoint to be successfully called.
+        Throws if maximum amount of endpoint calls is exceeded
+        """
+        retries = -1
+        url = f"http://localhost:{self.kc_config.container_healthcheck_port}/health/ready"
+
+        while retries < self.kc_config.max_healthcheck_retries:
+            try:
+                result = httpx.get(url=url, timeout=self.kc_config.healthcheck_retry_timeout)
+                if result.status_code == 200: return
+                else: retries += 1
+            except (TimeoutError, httpx.RemoteProtocolError) as e:
+                # httpx.RemoteProtocolError is thrown, while container is being prepared;
+                # this exception is handled separately with an explicit sleep during the retry timeout
+                if isinstance(e, httpx.RemoteProtocolError):
+                    sleep(self.kc_config.healthcheck_retry_timeout)
+                retries += 1
+            
+        raise TimeoutError("Failed to await for Keycloak server to be ready.")
+    
+    def __enter__(self) -> Self:
+        self.wait_for_server()
+        return self
+    
+    def __exit__(self, exc_type, exc_value, exc_tb) -> None:
+        pass
+    
+    @property
+    def app_client_id(self) -> str:
+        """ Fetches, caches and returns an internal UUID of app client. """
+        if self._app_client_id is None:
+            self._app_client_id = self.admin.get_client_id(self.kc_config.app_client_id)
+        
+        if self._app_client_id is None: raise Exception("Failed to retrieve app client ID.")
+        return self._app_client_id
+
+    def create_app_realm(self) -> None:
+        """ Creates app realm. """
+        self.admin.create_realm({
+            "realm": self.kc_config.app_realm_name,
+            "enabled": True
+        }, skip_exists=True)
+    
+    def delete_app_realm(self) -> None:
+        """ Deletes app realm. """
+        try:
+            self.admin.delete_realm(self.kc_config.app_realm_name)
+        except Exception:   # Ignore 404
+            pass
+    
+    @in_app_realm
+    def create_app_client(self) -> None:
+        """ Creates app client in the app realm and configures it. """
+        self.admin.create_client({
+            "clientId": self.kc_config.app_client_id,
+            "enabled": True,
+            "protocol": "openid-connect",
+
+            "publicClient": False,
+            "clientAuthenticatorType": "client-secret",
+            "secret": self.kc_config.app_client_secret,
+            "serviceAccountsEnabled": False,
+
+            "standardFlowEnabled": False,
+            "directAccessGrantsEnabled": True,
+        })
+    
+    @in_app_realm
+    def delete_app_client(self) -> None:
+        """Deletes app client in the app realm."""
+        self.admin.delete_client(self.app_client_id)
+
+    @in_app_realm
+    def add_client_role(self, name: str) -> None:
+        """Adds a client role with the given `name` to the app client in the app realm."""
+        self.admin.create_client_role(
+            self.app_client_id, {
+            "name": name,
+            "description": "",
+            "composite": False,
+            "clientRole": True,
+        })
+    
+    @in_app_realm
+    def delete_client_role(self, role_name: str) -> None:
+        """Deletes a client role with the given `role_name` for the app client in the app realm."""
+        self.admin.delete_client_role(self.app_client_id, role_name)
+
+    @in_app_realm
+    def add_user(self, username: str, password: str, app_client_roles: list[str]) -> str:
+        """
+        Creates a user in the app realm with the given attributes.
+        Assigns client roles to the user.
+        Returns the user ID.
+        """
+        user_id = self.admin.create_user({
+            "username": username,
+            "enabled": True,
+            "credentials": [{
+                "type": "password",
+                "value": password,
+                "temporary": False
+            }]
+        })
+
+        if app_client_roles:
+            self.assign_client_roles(user_id, app_client_roles)
+        
+        return user_id
+
+    @in_app_realm
+    def assign_client_roles(self, user_id: str, roles: list[str]) -> None:
+        """
+        Assigns the given client 'roles' to the specified `user_id`.
+        Roles must exist in the app client.
+        """
+        # Retrieve role representations to pass into the assignment API call
+        existing_roles = self.admin.get_client_roles(self.app_client_id)
+        roles_to_assign = [r for r in existing_roles if r["name"] in roles]
+        
+        if roles_to_assign:
+            self.admin.assign_client_role(user_id, self.app_client_id, roles_to_assign)
+
+    @in_app_realm
+    def delete_user(self, user_id: str) -> None:
+        """ Deletes a user with the given `user_id` in the app realm. """
+        self.admin.delete_user(user_id)
+
+# TODO delete
+if __name__ == "__main__":
+    from config import load_config
+    config = load_config()
+    reset_keycloak_app_realm(config.keycloak)
