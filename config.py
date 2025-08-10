5ed55c77e1fe11d2dc9e392954d0e50c382c3e51
-- <smoke571reg@yandex.ru>
2025-08-10 17:56:27 +0300

    Initial commit & config parsing

---
+++ config.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from pydantic import BaseModel, Field
+import yaml
+
+
+class KeycloakConfig(BaseModel):
+    container_name: str = Field(min_length=1)
+    container_main_port: int = Field(ge=1024, le=65535)
+    container_healthcheck_port: int = Field(ge=1024, le=65535)
+
+    max_healthcheck_retries: int = Field(ge=0)
+    healthcheck_retry_timeout: float = Field(ge=0)
+
+    admin_username: str = Field(min_length=1)
+    admin_password: str = Field(min_length=1)
+
+    app_realm_name: str = Field(min_length=1)
+    app_client_id: str = Field(min_length=1)
+    app_client_secret: str = Field(min_length=1)
+
+
+class Config(BaseModel):
+    keycloak: KeycloakConfig
+
+
+def load_config() -> Config:
+    path = Path(__file__).parent / "config.yml"
+    with open(path) as f:
+        data = yaml.safe_load(f)
+    return Config(**data)
+
