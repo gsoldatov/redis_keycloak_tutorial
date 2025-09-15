92571ee770e8aa260665695bb17aff048a7b1824
-- <smoke571reg@yandex.ru>
2025-09-15 18:10:29 +0300

    Added more key generators

--- src/redis/util.py
+++ src/redis/util.py
@@ -2,3 +2,19 @@ class RedisKeys:
     @staticmethod
     def user(username: str) -> str:
         return f"user:{username}"
+    
+    @staticmethod
+    def user_followers(username: str) -> str:
+        return f"user_followers:{username}"
+    
+    @staticmethod
+    def user_posts(username: str) -> str:
+        return f"user_posts:{username}"
+    
+    @staticmethod
+    def user_feed(username: str) -> str:
+        return f"user_feed:{username}"
+    
+    @staticmethod
+    def post(post_id: str | int) -> str:
+        return f"post:{post_id}"
